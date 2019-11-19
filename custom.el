0469e91de171af81167778c840aeab6f3f23589e
Gyb <54455365@qq.com>
2019-11-19 16:51:56 +0800

    commit

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote nil))
+ '(package-selected-packages
+   (quote
+    (flx-ido ido-completing-read+ yaml-mode window-numbering whitespace-cleanup-mode which-key uptimes tagedit switch-window smex session scratch restclient rainbow-mode rainbow-delimiters pip-requirements paredit-everywhere origami mmm-mode material-theme ivy-xref ipretty intellij-theme immortal-scratch ibuffer-vc ibuffer-projectile httprepl highlight-quoted highlight-parentheses fullframe flycheck-package flycheck-color-mode-line exec-path-from-shell elisp-slime-nav diredfl dimmer diminish diff-hl default-text-scale counsel company-quickhelp company-anaconda command-log-mode cl-libify cl-lib-highlight cask-mode better-defaults beacon auto-compile atom-one-dark-theme anzu aggressive-indent)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
